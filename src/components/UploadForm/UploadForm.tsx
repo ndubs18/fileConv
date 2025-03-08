921788bb6135053705d50cb2d32d98abfd2db2b9
ndubs18 <nick.winkler1@gmail.com>
2025-03-07 17:16:24 -0700

    configured mongo db and added functionality for added document on request sent to /process

--- src/components/UploadForm/UploadForm.tsx
+++ src/components/UploadForm/UploadForm.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-
+import { useRef, useState } from 'react';
+import { FileService } from '../../services/FileService';
 let styles = {
 	form: {
 		width: '200px'
@@ -9,31 +10,30 @@ let styles = {
 	}
 }
 
-let submitHandler = async (data: FormData) => {
-	try {
-		const res = await fetch('http://localhost:3000/upload',
-			{
-				method: 'POST',
-				body: data,
-			})
-		console.log(res);
-	} catch (error) {
-		console.log(error)
-	}
-}
-
-let uploadFile = () => {
-	/*TODO: This will utilize the file service
-	 *The file service is what communicates with the backend API
-	*/
-}
 let UploadForm = () => {
+	const fileService = useRef(new FileService);
+	const [fileData, setFileData] = useState(null);
+	const [processing, setProcessing] = useState(false);
+
+	let submitHandler = async (formData: FormData) => {
+		try {
+			setProcessing(true);
+			const uploadResponse = await fileService.current.uploadToS3(formData);
+			const data = uploadResponse.data;
+			setFileData(data);
+			const processingResponse = await fileService.current.processFile(data.uuid, data.filename);
+			console.log(processingResponse);
+		} catch (error) {
+			console.log(error)
+		}
+	}
 	return (
 		<form style={styles.form} action={submitHandler}>
 			<label htmlFor='fileUpload'>Upload a file:</label> <br></br>
 			<input type='file' id='fileUpload' accept='image/*, text/plain' name='file' />
 			<button type='submit' style={styles.submitBtn}>CONVERT</button>
 		</form >
+
 	)
 
 }
